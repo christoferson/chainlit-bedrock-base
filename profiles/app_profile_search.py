b3138f44f85b6fe09517e9c303ce1b356611307c
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-05-08 22:19:32 +0900

    Update search

--- profiles/app_profile_search.py
+++ profiles/app_profile_search.py
@@ -1,41 +1,78 @@
 import os
 import boto3
 import chainlit as cl
-from chainlit.input_widget import Select, Slider
+from chainlit.input_widget import Select, Slider, Switch
 import traceback
 import logging
 import app_bedrock
+import app_bedrock_lib
+
+from botocore.exceptions import ClientError
 
 AWS_REGION = os.environ["AWS_REGION"]
-AUTH_ADMIN_USR = os.environ["AUTH_ADMIN_USR"]
-AUTH_ADMIN_PWD = os.environ["AUTH_ADMIN_PWD"]
 
 bedrock = boto3.client("bedrock", region_name=AWS_REGION)
 bedrock_runtime = boto3.client('bedrock-runtime', region_name=AWS_REGION)
+bedrock_agent_runtime = boto3.client('bedrock-agent-runtime', region_name=AWS_REGION)
 
 async def on_chat_start():
+
+    kb_id_list = await app_bedrock_lib.list_knowledge_bases()
     
-    model_ids = ["anthropic.claude-3-sonnet-20240229-v1:0"]
+    model_ids = [
+        "anthropic.claude-v2", #"anthropic.claude-v2:0:18k",
+        "anthropic.claude-instant-v1",
+        "anthropic.claude-3-sonnet-20240229-v1:0",
+        "anthropic.claude-3-haiku-20240307-v1:0",
+        "amazon.titan-text-express-v1",
+        "mistral.mistral-7b-instruct-v0:2",
+        "mistral.mixtral-8x7b-instruct-v0:1",
+        "cohere.command-light-text-v14",
+        "cohere.command-text-v14",
+        #"meta.llama2-13b-chat-v1",
+        #"meta.llama2-70b-chat-v1",
+        "ai21.j2-mid"
+    ]
+    
     settings = await cl.ChatSettings(
         [
             Select(
-                id="Model",
-                label="Amazon Bedrock - Model",
-                values=model_ids,
-                initial_index=model_ids.index("anthropic.claude-3-sonnet-20240229-v1:0"),
-                
+                id="KnowledgeBase",
+                label="KnowledgeBase ID",
+                values=kb_id_list,
+                initial_index=0
             ),
             Slider(
-                id="Temperature",
-                label="Temperature",
-                initial=0.3,
-                min=0,
-                max=1,
-                step=0.1,
+                id = "RetrieveDocumentCount",
+                label = "KnowledgeBase DocumentCount",
+                initial = 3,
+                min = 1,
+                max = 8,
+                step = 1,
+            ),
+            Select(
+                id = "Mode",
+                label = "KnowledgeBase Generation Mode",
+                values = ["RetrieveAndGenerate", "Retrieve", "Generate"],
+                initial_index = 1,
+            ),
+            Select(
+                id = "Model",
+                label = "Foundation Model",
+                values = model_ids,
+                initial_index = model_ids.index("anthropic.claude-3-sonnet-20240229-v1:0"),
+            ),
+            Slider(
+                id = "Temperature",
+                label = "Temperature - Analytical vs Creative",
+                initial = 0.0,
+                min = 0,
+                max = 1,
+                step = 0.1,
             ),
             Slider(
                 id = "TopP",
-                label = "Top P",
+                label = "Top P - High Diversity vs Low Diversity",
                 initial = 1,
                 min = 0,
                 max = 1,
@@ -43,69 +80,79 @@ async def on_chat_start():
             ),
             Slider(
                 id = "TopK",
-                label = "Top K",
+                label = "Top K - High Probability vs Low Probability",
                 initial = 250,
                 min = 0,
-                max = 500,
+                max = 1500,
                 step = 5,
             ),
             Slider(
                 id="MaxTokenCount",
                 label="Max Token Size",
-                initial=2048,
-                min=256,
-                max=4096,
-                step=256,
+                initial = 2560,
+                min = 256,
+                max = 4096,
+                step = 256,
             ),
+            Switch(id="Strict", label="Retrieve - Limit Answers to KnowledgeBase", initial=False),
+            Switch(id="Terse", label="Terse - Terse & Consise Answers", initial=False),
+            Switch(id="SourceTableMarkdown", label="Source Tables Markdown Display", initial=True),
         ]
     ).send()
+
     await on_settings_update(settings)
 
 #@cl.on_settings_update
 async def on_settings_update(settings):
 
-    bedrock_model_id = settings["Model"]
+    knowledge_base_id = settings["KnowledgeBase"]
+    knowledge_base_id = knowledge_base_id.split(" ", 1)[0]
+    
+    llm_model_arn = "arn:aws:bedrock:{}::foundation-model/{}".format(AWS_REGION, settings["Model"])
+    #mode = settings["Mode"]
+    #strict = settings["Strict"]
+    kb_retrieve_document_count = int(settings["RetrieveDocumentCount"])
 
-    application_options = dict (
-        option_terse = False,
-        option_strict = False
-    )
+    bedrock_model_id = settings["Model"]
 
     inference_parameters = dict (
         temperature = settings["Temperature"],
         top_p = float(settings["TopP"]),
         top_k = int(settings["TopK"]),
         max_tokens_to_sample = int(settings["MaxTokenCount"]),
-        system_message = "You are a helpful assistant.",
-        stop_sequences =  []
+        stop_sequences =  [],
+        #system_message = "You are a helpful assistant. Unless instructed, omit any preamble and provide straight to the point concise answers."
+        system_message = "You are a helpful assistant and tries to answer questions as best as you can."
     )
 
-    model_strategy = app_bedrock.BedrockModelStrategyFactory.create(bedrock_model_id)
+    application_options = dict (
+        option_terse = settings["Terse"],
+        option_strict = settings["Strict"],
+        option_source_table_markdown_display = settings["SourceTableMarkdown"],
+    )
 
-    cl.user_session.set("bedrock_model_id", bedrock_model_id)
-    cl.user_session.set("application_options", application_options)
     cl.user_session.set("inference_parameters", inference_parameters)
-    cl.user_session.set("bedrock_model_strategy", model_strategy)
+    cl.user_session.set("bedrock_model_id", bedrock_model_id)
+    cl.user_session.set("llm_model_arn", llm_model_arn)
+    cl.user_session.set("knowledge_base_id", knowledge_base_id)
+    cl.user_session.set("kb_retrieve_document_count", kb_retrieve_document_count)
+    cl.user_session.set("application_options", application_options)
     
 
 #@cl.on_message
 async def on_message(message: cl.Message):
 
+    application_options = cl.user_session.get("application_options")
+    #session_id = cl.user_session.get("session_id") 
+    knowledge_base_id = cl.user_session.get("knowledge_base_id") 
+    #llm_model_arn = cl.user_session.get("llm_model_arn") 
     bedrock_model_id = cl.user_session.get("bedrock_model_id")
     inference_parameters = cl.user_session.get("inference_parameters")
-    application_options = cl.user_session.get("application_options")
-    bedrock_model_strategy : app_bedrock.BedrockModelStrategy = cl.user_session.get("bedrock_model_strategy")
+    option_strict = application_options.get("option_strict")
+    option_terse = application_options.get("option_terse")
+    kb_retrieve_document_count = cl.user_session.get("kb_retrieve_document_count")
 
-    #create_prompt(self, application_options: dict, context_info: str, query: str) -> str:
-    prompt_template = bedrock_model_strategy.create_prompt(application_options, "", message.content)
-    #prompt = prompt_template.replace("{input}", message.content)
-    #prompt = prompt.replace("{history}", "")
-    prompt = prompt_template
-    print(prompt)
-    #print(inference_parameters)
-    request = bedrock_model_strategy.create_request(inference_parameters, prompt)
-    #print(request)
-    print(f"{type(request)} {request}")
+    query = message.content
 
     msg = cl.Message(content="")
 
@@ -113,18 +160,103 @@ async def on_message(message: cl.Message):
 
     try:
 
-        #response = bedrock_runtime.invoke_model_with_response_stream(modelId = bedrock_model_id, body = json.dumps(request))
-        response = bedrock_model_strategy.send_request(request, bedrock_runtime, bedrock_model_id)
+        context_info = ""
 
-        #stream = response["body"]
-        #await bedrock_model_strategy.process_response_stream(stream, msg)
-        await bedrock_model_strategy.process_response(response, msg)
+        async with cl.Step(name="KnowledgeBase", type="llm", root=False) as step:
+            step.input = msg.content
+
+            await step.stream_token(f"\nSearch Max {kb_retrieve_document_count} documents.\n")
+
+            try:
+
+                prompt = f"""\n\nHuman: {query[0:900]}
+                Assistant:
+                """
+
+                response = bedrock_agent_runtime.retrieve(
+                    knowledgeBaseId = knowledge_base_id,
+                    retrievalQuery={
+                        'text': prompt,
+                    },
+                    retrievalConfiguration={
+                        'vectorSearchConfiguration': {
+                            'numberOfResults': kb_retrieve_document_count
+                        }
+                    }
+                )
+
+                reference_elements = []
+                for i, retrievalResult in enumerate(response['retrievalResults']):
+                    uri = retrievalResult['location']['s3Location']['uri']
+                    text = retrievalResult['content']['text']
+                    excerpt = text[0:75]
+                    score = retrievalResult['score']
+                    print(f"{i} RetrievalResult: {score} {uri} {excerpt}")
+                    #await msg.stream_token(f"\n{i} RetrievalResult: {score} {uri} {excerpt}\n")
+                    context_info += f"{text}\n" #context_info += f"<p>${text}</p>\n" #context_info += f"${text}\n"
+                    #await step.stream_token(f"\n[{i+1}] score={score} uri={uri} len={len(text)} text={excerpt}\n")
+                    await step.stream_token(f"\n[{i+1}] score={score} uri={uri} len={len(text)}\n")
+                    reference_elements.append(cl.Text(name=f"[{i+1}] {uri}", content=text, display="inline"))
+                
+                await step.stream_token(f"\n")
+                step.elements = reference_elements
+
+            except Exception as e:
+                logging.error(traceback.format_exc())
+                await msg.stream_token(f"{e}")
+            finally:
+                await step.send()
+
+        async with cl.Step(name="Model", type="llm", root=False) as step_llm:
+            step_llm.input = msg.content
+
+            elements = []
+
+            try:
+
+                bedrock_model_strategy = app_bedrock.BedrockModelStrategyFactory.create(bedrock_model_id)
+
+                # Create Prompt create_prompt(self, application_options: dict, context_info: str, query: str) -> str:
+
+                prompt = bedrock_model_strategy.create_prompt(application_options, context_info, query)
+
+                # End - Create Prompt 
+
+                system_message = inference_parameters.get("system_message")
+                elements.append(cl.Text(name=f"system", content=system_message.replace("\n\n", "").rstrip(), display="inline")) 
+
+                elements.append(cl.Text(name=f"prompt", content=prompt.replace("\n\n", "").rstrip(), display="inline"))
+
+                max_tokens = inference_parameters.get("max_tokens_to_sample")
+                temperature = inference_parameters.get('temperature')
+                await step_llm.stream_token(f"model.id={bedrock_model_id} prompt.len={len(prompt)} temperature={temperature} max_tokens={max_tokens}")
+
+                request = bedrock_model_strategy.create_request(inference_parameters, prompt)
+ 
+                print(f"{type(request)} {request}")
+
+                response = bedrock_model_strategy.send_request(request, bedrock_runtime, bedrock_model_id)
+
+                await bedrock_model_strategy.process_response(response, msg)
+
+                step_llm.elements = elements
+
+            except ClientError as err:
+                message = err.response["Error"]["Message"]
+                logging.error("A client error occurred: %s", message)
+                await msg.stream_token(f"{message}")
+            except Exception as e:
+                logging.error(traceback.format_exc())
+                await msg.stream_token(f"{e}")
+            finally:
+                await step_llm.send()
+
+        await msg.stream_token(f". strict={option_strict} terse={option_terse}\n")
+        await msg.send()
 
     except Exception as e:
         logging.error(traceback.format_exc())
         await msg.stream_token(f"{e}")
     finally:
         await msg.send()
-
-    print("End")
 
