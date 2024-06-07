3ca3ca06a16784c28ed90e847679932fe9466e6a
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-06-08 00:09:24 +0900

    Add top k

--- profiles/app_profile_data.py
+++ profiles/app_profile_data.py
@@ -6,7 +6,6 @@ import traceback
 import logging
 import app_bedrock
 import app_bedrock_lib
-#import uuid
 
 AWS_REGION = os.environ["AWS_REGION"]
 AUTH_ADMIN_USR = os.environ["AUTH_ADMIN_USR"]
@@ -28,12 +27,19 @@ async def on_chat_start():
                 values=kb_id_list,
                 initial_index=0
             ),
+            Slider(
+                id = "RetrieveDocumentCount",
+                label = "KnowledgeBase DocumentCount",
+                initial = 8,
+                min = 1,
+                max = 32,
+                step = 1,
+            ),
             Select(
                 id="Model",
                 label="Amazon Bedrock - Model",
                 values=model_ids,
                 initial_index=model_ids.index("anthropic.claude-3-sonnet-20240229-v1:0"),
-                
             ),
             Slider(
                 id="Temperature",
@@ -80,13 +86,13 @@ async def on_settings_update(settings):
     
     llm_model_arn = "arn:aws:bedrock:{}::foundation-model/{}".format(AWS_REGION, settings["Model"])
 
-
     application_options = dict (
         option_terse = False,
         option_strict = False
     )
 
     inference_parameters = dict (
+        kb_retrieve_document_count = int(settings["RetrieveDocumentCount"]),
         temperature = settings["Temperature"],
         top_p = float(settings["TopP"]),
         top_k = int(settings["TopK"]),
@@ -95,9 +101,7 @@ async def on_settings_update(settings):
         stop_sequences =  []
     )
 
-     #BedrockModelStrategy()    
     model_strategy = app_bedrock.BedrockModelStrategyFactory.create(bedrock_model_id)
-
 
     cl.user_session.set("bedrock_model_id", bedrock_model_id)
     cl.user_session.set("llm_model_arn", llm_model_arn)
@@ -123,11 +127,11 @@ async def on_message(message: cl.Message):
     #prompt = prompt_template.replace("{input}", message.content)
     #prompt = prompt.replace("{history}", "")
     prompt = prompt_template
-    print(prompt)
+    #print(prompt)
     #print(inference_parameters)
     request = bedrock_model_strategy.create_request(inference_parameters, prompt)
     #print(request)
-    print(f"{type(request)} {request}")
+    #print(f"{type(request)} {request}")
 
 
     msg = cl.Message(content="")
@@ -147,10 +151,22 @@ async def on_message(message: cl.Message):
                     'modelArn': llm_model_arn,
                     'retrievalConfiguration': {
                         'vectorSearchConfiguration': {
-                            'numberOfResults': 3, #kb_retrieve_document_count,  #Minimum value of 1. Maximum value of 100
+                            'numberOfResults': inference_parameters.get('kb_retrieve_document_count'), #kb_retrieve_document_count,  #Minimum value of 1. Maximum value of 100
                             #'overrideSearchType': 'HYBRID'|'SEMANTIC'
                         }
                     },
+                    'generationConfiguration': {
+                        'inferenceConfig': {
+                            'textInferenceConfig': {
+                                'maxTokens': inference_parameters.get("max_tokens_to_sample"),
+                                'temperature': inference_parameters.get('temperature'),
+                                'topP': inference_parameters.get('top_p'),
+                            }
+                        }
+                    },
+                    'additionalModelRequestFields': {
+                        "top_k": inference_parameters.get('top_k')
+                    }
                     # Unknown parameter in retrieveAndGenerateConfiguration.knowledgeBaseConfiguration: "generationConfiguration", must be one of: knowledgeBaseId, modelArn, retrievalConfiguration
                     #'generationConfiguration': {
                     #    'promptTemplate': {
@@ -163,6 +179,8 @@ async def on_message(message: cl.Message):
 
         if session_id != "" and session_id is not None:
             params["sessionId"] = session_id #session_id=84219eab-2060-4a8f-a481-3356d66b8586
+
+        print(params)
 
         response = bedrock_agent_runtime.retrieve_and_generate(
             **params
