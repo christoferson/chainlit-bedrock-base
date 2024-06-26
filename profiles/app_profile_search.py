702a8a9e134551fb93ec4998cbcf879fa494cacd
christoferson <sweetsorrow1981@yahoo.co.jp>
2024-06-26 23:10:51 +0900

    tool

--- profiles/app_profile_search.py
+++ profiles/app_profile_search.py
@@ -138,6 +138,122 @@ async def on_settings_update(settings):
     cl.user_session.set("kb_retrieve_document_count", kb_retrieve_document_count)
     cl.user_session.set("application_options", application_options)
     
+@cl.step(name="retrieve", type="tool")
+async def tool(query:str):
+
+    application_options = cl.user_session.get("application_options")
+    knowledge_base_id = cl.user_session.get("knowledge_base_id") 
+    kb_retrieve_document_count = cl.user_session.get("kb_retrieve_document_count")
+
+    current_step = cl.context.current_step
+    
+    await current_step.stream_token(f"\nSearch Max {kb_retrieve_document_count} documents.\n")
+
+    context_info = ""
+
+    try:
+
+        prompt = f"""\n\nHuman: {query[0:900]}
+        Assistant:
+        """
+
+        response = bedrock_agent_runtime.retrieve(
+            knowledgeBaseId = knowledge_base_id,
+            retrievalQuery={
+                'text': prompt,
+            },
+            retrievalConfiguration={
+                'vectorSearchConfiguration': {
+                    'numberOfResults': kb_retrieve_document_count
+                }
+            }
+        )
+
+        reference_elements = []
+        for i, retrievalResult in enumerate(response['retrievalResults']):
+            uri = retrievalResult['location']['s3Location']['uri']
+            text = retrievalResult['content']['text']
+            excerpt = text[0:75]
+            score = retrievalResult['score']
+            print(f"{i} RetrievalResult: {score} {uri} {excerpt}")
+            #await msg.stream_token(f"\n{i} RetrievalResult: {score} {uri} {excerpt}\n")
+            context_info += f"{text}\n" #context_info += f"<p>${text}</p>\n" #context_info += f"${text}\n"
+            #await step.stream_token(f"\n[{i+1}] score={score} uri={uri} len={len(text)} text={excerpt}\n")
+            await current_step.stream_token(f"\n[{i+1}] score={score} uri={uri} len={len(text)}\n")
+            reference_elements.append(cl.Text(name=f"[{i+1}] {uri}", content=text, display="inline")) # “side” (default), “inline”, or “page”
+        
+        await current_step.stream_token(f"\n")
+        current_step.elements = reference_elements
+
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        await current_step.stream_token(f"{e}")
+    finally:
+        await current_step.send()
+
+    return context_info
+
+@cl.step(name="generate", type="tool")
+async def generate(context_info:str, query:str, msg:cl.Message):
+
+    application_options = cl.user_session.get("application_options")
+    application_options = cl.user_session.get("application_options")
+    #session_id = cl.user_session.get("session_id") 
+    knowledge_base_id = cl.user_session.get("knowledge_base_id") 
+    #llm_model_arn = cl.user_session.get("llm_model_arn") 
+    bedrock_model_id = cl.user_session.get("bedrock_model_id")
+    inference_parameters = cl.user_session.get("inference_parameters")
+    option_strict = application_options.get("option_strict")
+    option_terse = application_options.get("option_terse")
+    kb_retrieve_document_count = cl.user_session.get("kb_retrieve_document_count")
+
+    current_step = cl.context.current_step
+    
+    elements = []
+
+    try:
+
+        bedrock_model_strategy = app_bedrock.BedrockModelStrategyFactory.create(bedrock_model_id)
+
+        # Create Prompt create_prompt(self, application_options: dict, context_info: str, query: str) -> str:
+
+        prompt = bedrock_model_strategy.create_prompt(application_options, context_info, query)
+
+        # End - Create Prompt 
+
+        system_message = inference_parameters.get("system_message")
+        elements.append(cl.Text(name=f"system", content=system_message.replace("\n\n", "").rstrip(), display="inline")) 
+
+        elements.append(cl.Text(name=f"prompt", content=prompt.replace("\n\n", "").rstrip(), display="inline"))
+
+        max_tokens = inference_parameters.get("max_tokens_to_sample")
+        temperature = inference_parameters.get('temperature')
+        await current_step.stream_token(f"model.id={bedrock_model_id} prompt.len={len(prompt)} temperature={temperature} max_tokens={max_tokens}")
+
+        request = bedrock_model_strategy.create_request(inference_parameters, prompt)
+
+        print(f"{type(request)} {request}")
+
+        response = bedrock_model_strategy.send_request(request, bedrock_runtime, bedrock_model_id)
+
+        await bedrock_model_strategy.process_response(response, msg)
+
+        current_step.elements = elements
+
+    except ClientError as err:
+        message = err.response["Error"]["Message"]
+        logging.error("A client error occurred: %s", message)
+        await current_step.stream_token(f"{message}")
+    except Exception as e:
+        logging.error(traceback.format_exc())
+        await current_step.stream_token(f"{e}")
+    finally:
+        await current_step.send()
+
+    await current_step.stream_token(f". strict={option_strict} terse={option_terse}\n")
+    await current_step.send()
+
+    return "Response from the tool!"
 
 #@cl.on_message
 async def on_message(message: cl.Message):
@@ -162,96 +278,10 @@ async def on_message(message: cl.Message):
 
         context_info = ""
 
-        async with cl.Step(name="KnowledgeBase", type="llm", root=False) as step:
-            step.input = msg.content
+        context_info = await tool(query)
+        
+        await generate(context_info, query, msg)
 
-            await step.stream_token(f"\nSearch Max {kb_retrieve_document_count} documents.\n")
-
-            try:
-
-                prompt = f"""\n\nHuman: {query[0:900]}
-                Assistant:
-                """
-
-                response = bedrock_agent_runtime.retrieve(
-                    knowledgeBaseId = knowledge_base_id,
-                    retrievalQuery={
-                        'text': prompt,
-                    },
-                    retrievalConfiguration={
-                        'vectorSearchConfiguration': {
-                            'numberOfResults': kb_retrieve_document_count
-                        }
-                    }
-                )
-
-                reference_elements = []
-                for i, retrievalResult in enumerate(response['retrievalResults']):
-                    uri = retrievalResult['location']['s3Location']['uri']
-                    text = retrievalResult['content']['text']
-                    excerpt = text[0:75]
-                    score = retrievalResult['score']
-                    print(f"{i} RetrievalResult: {score} {uri} {excerpt}")
-                    #await msg.stream_token(f"\n{i} RetrievalResult: {score} {uri} {excerpt}\n")
-                    context_info += f"{text}\n" #context_info += f"<p>${text}</p>\n" #context_info += f"${text}\n"
-                    #await step.stream_token(f"\n[{i+1}] score={score} uri={uri} len={len(text)} text={excerpt}\n")
-                    await step.stream_token(f"\n[{i+1}] score={score} uri={uri} len={len(text)}\n")
-                    reference_elements.append(cl.Text(name=f"[{i+1}] {uri}", content=text, display="inline"))
-                
-                await step.stream_token(f"\n")
-                step.elements = reference_elements
-
-            except Exception as e:
-                logging.error(traceback.format_exc())
-                await msg.stream_token(f"{e}")
-            finally:
-                await step.send()
-
-        async with cl.Step(name="Model", type="llm", root=False) as step_llm:
-            step_llm.input = msg.content
-
-            elements = []
-
-            try:
-
-                bedrock_model_strategy = app_bedrock.BedrockModelStrategyFactory.create(bedrock_model_id)
-
-                # Create Prompt create_prompt(self, application_options: dict, context_info: str, query: str) -> str:
-
-                prompt = bedrock_model_strategy.create_prompt(application_options, context_info, query)
-
-                # End - Create Prompt 
-
-                system_message = inference_parameters.get("system_message")
-                elements.append(cl.Text(name=f"system", content=system_message.replace("\n\n", "").rstrip(), display="inline")) 
-
-                elements.append(cl.Text(name=f"prompt", content=prompt.replace("\n\n", "").rstrip(), display="inline"))
-
-                max_tokens = inference_parameters.get("max_tokens_to_sample")
-                temperature = inference_parameters.get('temperature')
-                await step_llm.stream_token(f"model.id={bedrock_model_id} prompt.len={len(prompt)} temperature={temperature} max_tokens={max_tokens}")
-
-                request = bedrock_model_strategy.create_request(inference_parameters, prompt)
- 
-                print(f"{type(request)} {request}")
-
-                response = bedrock_model_strategy.send_request(request, bedrock_runtime, bedrock_model_id)
-
-                await bedrock_model_strategy.process_response(response, msg)
-
-                step_llm.elements = elements
-
-            except ClientError as err:
-                message = err.response["Error"]["Message"]
-                logging.error("A client error occurred: %s", message)
-                await msg.stream_token(f"{message}")
-            except Exception as e:
-                logging.error(traceback.format_exc())
-                await msg.stream_token(f"{e}")
-            finally:
-                await step_llm.send()
-
-        await msg.stream_token(f". strict={option_strict} terse={option_terse}\n")
         await msg.send()
 
     except Exception as e:
